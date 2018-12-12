07385223277fa51697acf23af7cb6020f5588875
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2018-12-12 09:56:36 +0900

    Serverもビルドできるようにした

--- Assets/Editor/BatchBuild/BatchBuild.cs
+++ Assets/Editor/BatchBuild/BatchBuild.cs
@@ -6,16 +6,27 @@ namespace XFlag.Alter3SimulatorEditor
 {
     public static class BatchBuild
     {
-        [MenuItem("Window/Alter3/Build Test Client")]
+        [MenuItem("Window/Alter3/Build/Test Client")]
         private static void BuildTestClient()
         {
-            var scene = EditorBuildSettings.scenes.First(s => Path.GetFileNameWithoutExtension(s.path) == "Client");
+            BuildSingleScene("Client", "TestClient");
+        }
+
+        [MenuItem("Window/Alter3/Build/Server")]
+        private static void BuildServer()
+        {
+            BuildSingleScene("Server", "Alter3Simulator");
+        }
+
+        private static void BuildSingleScene(string sceneName, string outputName)
+        {
+            var scene = EditorBuildSettings.scenes.First(s => Path.GetFileNameWithoutExtension(s.path) == sceneName);
             var buildPlayerOptions = new BuildPlayerOptions
             {
                 scenes = new[] { scene.path },
                 targetGroup = BuildTargetGroup.Standalone,
                 target = BuildTarget.StandaloneWindows64,
-                locationPathName = "Builds/Windows/TestClient.exe",
+                locationPathName = $"Builds/Windows/{outputName}/{outputName}.exe",
                 options = BuildOptions.None
             };
             BuildPipeline.BuildPlayer(buildPlayerOptions);

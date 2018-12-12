46c670b61155fe75ab1df6d49f4c800b558ee8ff
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2018-12-12 14:55:52 +0900

    OSXのビルド対応

--- UnityProject/Alter3/Assets/Alter3/Editor/BatchBuild/BatchBuild.cs
+++ UnityProject/Alter3/Assets/Alter3/Editor/BatchBuild/BatchBuild.cs
@@ -6,30 +6,54 @@ namespace XFlag.Alter3SimulatorEditor
 {
     public static class BatchBuild
     {
-        [MenuItem("Window/Alter3/Build/Test Client")]
-        private static void BuildTestClient()
+        [MenuItem("Window/Alter3/Build/Test Client (Win64)")]
+        private static void BuildTestClient_Win64()
         {
-            BuildSingleScene("Client", "TestClient");
+            BuildSingleScene("Client", "TestClient", BuildTarget.StandaloneWindows64);
         }
 
-        [MenuItem("Window/Alter3/Build/Server")]
-        private static void BuildServer()
+        [MenuItem("Window/Alter3/Build/Test Client (OSX)")]
+        private static void BuildTestClient_OSX()
         {
-            BuildSingleScene("Server", "Alter3Simulator");
+            BuildSingleScene("Client", "TestClient", BuildTarget.StandaloneOSX);
         }
 
-        private static void BuildSingleScene(string sceneName, string outputName)
+        [MenuItem("Window/Alter3/Build/Server (Win64)")]
+        private static void BuildServer_Win64()
+        {
+            BuildSingleScene("Server", "Alter3Simulator", BuildTarget.StandaloneWindows64);
+        }
+
+        [MenuItem("Window/Alter3/Build/Server (OSX)")]
+        private static void BuildServer_OSX()
+        {
+            BuildSingleScene("Server", "Alter3Simulator", BuildTarget.StandaloneOSX);
+        }
+
+        private static void BuildSingleScene(string sceneName, string outputName, BuildTarget target)
         {
             var scene = EditorBuildSettings.scenes.First(s => Path.GetFileNameWithoutExtension(s.path) == sceneName);
             var buildPlayerOptions = new BuildPlayerOptions
             {
                 scenes = new[] { scene.path },
                 targetGroup = BuildTargetGroup.Standalone,
-                target = BuildTarget.StandaloneWindows64,
-                locationPathName = $"Builds/Windows/{outputName}/{outputName}.exe",
+                target = target,
+                locationPathName = $"Builds/{target}/{outputName}/{outputName}{GetExtension(target)}",
                 options = BuildOptions.None
             };
             BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        private static string GetExtension(BuildTarget target)
+        {
+            switch (target)
+            {
+                case BuildTarget.StandaloneOSX:
+                    return ".app";
+                case BuildTarget.StandaloneWindows64:
+                    return ".exe";
+            }
+            return "";
         }
     }
 }

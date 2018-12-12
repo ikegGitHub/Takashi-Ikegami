ac2754dc4e35c65803de8b5817c95cd9580b4231
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2018-12-12 09:42:02 +0900

    TestClientのバッチビルド対応

---
+++ Assets/Editor/BatchBuild/BatchBuild.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+
+namespace XFlag.Alter3SimulatorEditor
+{
+    public static class BatchBuild
+    {
+        [MenuItem("Window/Alter3/Build Test Client")]
+        private static void BuildTestClient()
+        {
+            var scene = EditorBuildSettings.scenes.First(s => Path.GetFileNameWithoutExtension(s.path) == "Client");
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = new[] { scene.path },
+                targetGroup = BuildTargetGroup.Standalone,
+                target = BuildTarget.StandaloneWindows64,
+                locationPathName = "Builds/Windows/TestClient.exe",
+                options = BuildOptions.None
+            };
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+    }
+}

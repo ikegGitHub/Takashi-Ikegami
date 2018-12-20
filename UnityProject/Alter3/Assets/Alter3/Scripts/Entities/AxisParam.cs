157ae7927c0cf95268a1e68a9f5a06556694ee80
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2018-12-20 19:16:52 +0900

    AxisParamの等価比較周りの実装

--- UnityProject/Alter3/Assets/Alter3/Scripts/Entities/AxisParam.cs
+++ UnityProject/Alter3/Assets/Alter3/Scripts/Entities/AxisParam.cs
@@ -1,6 +1,9 @@
-﻿namespace XFlag.Alter3Simulator
+﻿using System;
+using System.Collections.Generic;
+
+namespace XFlag.Alter3Simulator
 {
-    public class AxisParam
+    public class AxisParam : IEquatable<AxisParam>
     {
         public int AxisNumber { get; set; }
 
@@ -10,9 +13,43 @@
 
         public int Duration { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as AxisParam);
+        }
+
+        public bool Equals(AxisParam other)
+        {
+            return other != null &&
+                   AxisNumber == other.AxisNumber &&
+                   Value == other.Value &&
+                   Priority == other.Priority &&
+                   Duration == other.Duration;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1490284165;
+            hashCode = hashCode * -1521134295 + AxisNumber.GetHashCode();
+            hashCode = hashCode * -1521134295 + Value.GetHashCode();
+            hashCode = hashCode * -1521134295 + Priority.GetHashCode();
+            hashCode = hashCode * -1521134295 + Duration.GetHashCode();
+            return hashCode;
+        }
+
         public override string ToString()
         {
-            return $"{GetType()}{{AxisNumber={AxisNumber},Value={Value},Priority={Priority},Duration={Duration}}}";
+            return $"{nameof(AxisParam)}{{{nameof(AxisNumber)}={AxisNumber},{nameof(Value)}={Value},{nameof(Priority)}={Priority},{nameof(Duration)}={Duration}}}";
+        }
+
+        public static bool operator ==(AxisParam param1, AxisParam param2)
+        {
+            return EqualityComparer<AxisParam>.Default.Equals(param1, param2);
+        }
+
+        public static bool operator !=(AxisParam param1, AxisParam param2)
+        {
+            return !(param1 == param2);
         }
     }
 }

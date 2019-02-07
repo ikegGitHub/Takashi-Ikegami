b3dc19c146abefc5b3b7d9e1cd8302d0fb93dc98
Akihiro Yoshida <akihiro.yoshida@mixi.co.jp>
2019-02-07 11:05:08 +0900

    ステージを追加（Alter3仮配置）

---
+++ UnityProject/Alter3/Assets/Alter3/Scripts/Application/Stage/StageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace XFlag.Alter3Simulator
+{
+    [DisallowMultipleComponent]
+
+    public class StageController : MonoBehaviour
+    {
+        [SerializeField]
+        protected GameObject StageGameObject = null;
+
+
+    }
+}

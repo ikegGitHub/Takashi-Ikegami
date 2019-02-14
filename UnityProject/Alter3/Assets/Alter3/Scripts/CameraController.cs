8b5eca276e671a784d77a26be1b58d7d72ce927a
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-02-14 18:02:08 +0900

    カメラの挙動を自然な動作に修正

--- UnityProject/Alter3/Assets/Alter3/Scripts/CameraController.cs
+++ UnityProject/Alter3/Assets/Alter3/Scripts/CameraController.cs
@@ -7,59 +7,18 @@ namespace XFlag.Alter3Simulator
     public class CameraController : MonoBehaviour
     {
         [SerializeField]
-        private Transform _cameraTransform = null;
-
-        [SerializeField]
-        private Transform _lookAtTransform = null;
-
-        [SerializeField]
-        private float _distanceMin = 1.0f;
-
-        [SerializeField]
-        private float _distanceMax = 10.0f;
-
-        [SerializeField]
         private DragAreaView _dragAreView = null;
-
-        private float _distance;
-        private float _yAngle;
-        private float _xAngle;
-
-        public void Reset()
-        {
-            _distance = 1.0f;
-            _xAngle = 0;
-            _yAngle = 0;
-
-            transform.localScale = Vector3.one;
-            transform.localRotation = Quaternion.identity;
-
-            UpdateLookAt();
-        }
 
         private void Awake()
         {
             _dragAreView.OnDrag += OnDrag;
-
-            Reset();
+            _dragAreView.OnScroll += OnScroll;
         }
 
         private void OnDestroy()
         {
             _dragAreView.OnDrag -= OnDrag;
-        }
-
-        private void Update()
-        {
-            _distance = Mathf.Clamp(_distance + Input.mouseScrollDelta.y * 0.1f, _distanceMin, _distanceMax);
-            transform.localScale = new Vector3(1, 1, _distance);
-        }
-
-        private void UpdateLookAt()
-        {
-            transform.localRotation = Quaternion.Euler(_xAngle, _yAngle, 0);
-
-            _cameraTransform.LookAt(_lookAtTransform, Vector3.up);
+            _dragAreView.OnScroll -= OnScroll;
         }
 
         private void OnDrag(PointerEventData eventData)
@@ -70,17 +29,19 @@ namespace XFlag.Alter3Simulator
                 float transSpeed = 0.2f;
                 var delta2 = new Vector2(delta.x, -delta.y);
                 transform.Translate(delta2 * Time.deltaTime * transSpeed);
-                _lookAtTransform.Translate(delta2 * Time.deltaTime * transSpeed);
-
             }
             else
             {
-                _yAngle = Mathf.Repeat(_yAngle + delta.x * 0.15f, 360);
-                _xAngle = Mathf.Repeat(_xAngle + delta.y * 0.15f, 360);
-
-                UpdateLookAt();
-
+                var angles = transform.localEulerAngles;
+                angles.y = Mathf.Repeat(angles.y + delta.x * 0.15f, 360);
+                angles.x = Mathf.Repeat(angles.x + delta.y * 0.15f, 360);
+                transform.localEulerAngles = angles;
             }
+        }
+
+        private void OnScroll(PointerEventData eventData)
+        {
+            transform.Translate(0, 0, eventData.scrollDelta.y);
         }
     }
 }

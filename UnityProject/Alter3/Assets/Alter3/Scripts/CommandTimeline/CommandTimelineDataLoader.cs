93fd934d8302ed3d3f5818c1440e93ff891bb0db
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-02-25 20:50:49 +0900

    コマンドタイムラインのモデル、ローダー、プレイヤーを実装

---
+++ UnityProject/Alter3/Assets/Alter3/Scripts/CommandTimeline/CommandTimelineDataLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace XFlag.Alter3Simulator
+{
+    public class CommandTimelineDataLoader : IDisposable
+    {
+        private BinaryReader _reader;
+
+        public CommandTimelineDataLoader(BinaryReader reader)
+        {
+            _reader = reader;
+        }
+
+        public CommandTimelineRecord[] Load()
+        {
+            var count = _reader.ReadInt32();
+
+            var commands = new CommandTimelineRecord[count];
+            for (int i = 0; i < count; i++)
+            {
+                var microSeconds = _reader.ReadInt64();
+                var axisNumber = _reader.ReadByte();
+                var value = _reader.ReadByte();
+                commands[i] = new CommandTimelineRecord(microSeconds, axisNumber, value);
+            }
+            return commands;
+        }
+
+        void IDisposable.Dispose()
+        {
+            _reader.Dispose();
+        }
+    }
+}

f5c2e0cbf7d852c95bd26a1a8812f5852181433b
Akihiro Yoshida <akihiro.yoshida@mixi.co.jp>
2019-03-06 18:48:56 +0900

    仮カメラ画像のキャプチャ追加、目のカメラの情報に左右の情報を追加

--- UnityProject/Alter3/Assets/Alter3/Scripts/Application/EyeCamera/Alter3EveCameraController.cs
+++ UnityProject/Alter3/Assets/Alter3/Scripts/Application/EyeCamera/Alter3EveCameraController.cs
@@ -8,11 +8,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+using System.Collections;
+using System.IO;
+
 namespace XFlag.Alter3Simulator
 {
     [DisallowMultipleComponent]
     public class Alter3EveCameraController : MonoBehaviour
     {
+
         protected RenderTexture renderTexture = null;
         public RenderTexture RenderTexture
         {
@@ -29,6 +33,15 @@ namespace XFlag.Alter3Simulator
         [SerializeField]
         protected Camera camera = null;
 
+
+        protected EyeCameraPos eyePos = EyeCameraPos.Left;
+        public EyeCameraPos EyeCameraPos
+        {
+            get { return this.eyePos; }
+            set { this.eyePos = value; }
+        }
+
+
         private void Awake()
         {
             renderTexture = new RenderTexture(resolutionX, resolutionY, 24, RenderTextureFormat.ARGB32);
@@ -36,6 +49,39 @@ namespace XFlag.Alter3Simulator
             camera.targetTexture = renderTexture;
         }
 
+        int framecount = 0;
 
+        void SaveTexture()
+        {
+            Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+            RenderTexture.active = renderTexture;
+            tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            tex.Apply();
+
+            byte[] bytes = tex.EncodeToPNG();
+            Destroy(tex);
+
+            string path = Application.dataPath + "/Capture";
+            path = Path.Combine(path, "Screen" + framecount.ToString() + ".png");
+
+            if (!Directory.Exists(Path.GetDirectoryName(path)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+            File.WriteAllBytes(path, bytes);
+
+
+
+
+        }
+
+
+        private void Update()
+        {
+#if !UNITY_EDITOR
+            SaveTexture();
+#endif
+            framecount++;
+        }
     }
 }

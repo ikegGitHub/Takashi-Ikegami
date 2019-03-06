3316854518e1d25ae828cbea73baf5dfc626e87e
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-03-07 06:01:26 +0900

    ジョイントの更新処理をJointParameterの責務にしてみた

--- UnityProject/Alter3/Assets/Alter3/Scripts/Application/JointParameter.cs
+++ UnityProject/Alter3/Assets/Alter3/Scripts/Application/JointParameter.cs
@@ -54,7 +54,66 @@ namespace XFlag.Alter3Simulator
 
 
         }
+
+        public void UpdateTargetRotation(JointItem jointItem, float normalizedValue)
+        {
+            var ang = Mathf.Lerp(jointItem.rangeMin, jointItem.rangeMax, normalizedValue);
+
+            StartRotation = CurrentRotation;
+            var newRotation = jointItem.Axis * ang;
+
+            float ax = 0;
+            float ay = 0;
+            float az = 0;
+            Quaternion qx = Quaternion.identity;
+            Quaternion qy = Quaternion.identity;
+            Quaternion qz = Quaternion.identity;
+            if (jointItem.Axis.x != 0)
+            {
+                qx = Quaternion.AngleAxis(newRotation.x, Vector3.right);
+                ax = newRotation.x;
+            }
+            else
+            {
+                qx = Quaternion.AngleAxis(CurrentRotation.x, Vector3.right);
+                ax = CurrentRotation.x;
+            }
+            if (jointItem.Axis.z != 0)
+            {
+                qz = Quaternion.AngleAxis(newRotation.z, Vector3.forward);
+                az = newRotation.z;
+            }
+            else
+            {
+                qz = Quaternion.AngleAxis(CurrentRotation.z, Vector3.forward);
+                az = CurrentRotation.z;
+            }
+            if (jointItem.Axis.y != 0)
+            {
+                qy = Quaternion.AngleAxis(newRotation.y, Vector3.up);
+                ay = newRotation.y;
+            }
+            else
+            {
+                qy = Quaternion.AngleAxis(CurrentRotation.y, Vector3.up);
+                ay = CurrentRotation.y;
+            }
+            NextQuat = qx * qz * qy;
+            NextRotation = new Vector3(ax, ay, az);
+        }
+
+        public void Update(float spring, float damper, float dt)
+        {
+            // なんとなくスプリング＋ダンパー
+            var acceleration = ((NextRotation - CurrentRotation) * spring) - (Velocity * damper);
+            Velocity += acceleration * dt;
+            CurrentRotation += Velocity * dt;
+
+            var qx = Quaternion.AngleAxis(CurrentRotation.x, Vector3.right);
+            var qy = Quaternion.AngleAxis(CurrentRotation.y, Vector3.up);
+            var qz = Quaternion.AngleAxis(CurrentRotation.z, Vector3.forward);
+
+            Transform.localRotation = qx * qz * qy;
+        }
     }
 }
-
-

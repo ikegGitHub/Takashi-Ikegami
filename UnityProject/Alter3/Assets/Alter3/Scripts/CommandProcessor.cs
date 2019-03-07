5c2d93872bb5a9526d68efce324151aecae14f6a
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-03-08 00:12:49 +0900

    VisitorをやめてType-Switchにした

--- UnityProject/Alter3/Assets/Alter3/Scripts/CommandProcessor.cs
+++ UnityProject/Alter3/Assets/Alter3/Scripts/CommandProcessor.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using XFlag.Alter3Simulator.Network;
 
 namespace XFlag.Alter3Simulator
 {
-    public class CommandProcessor : CommandVisitorBase
+    public class CommandProcessor
     {
         private static readonly CommandParser _commandParser = new CommandParser();
 
@@ -28,7 +26,7 @@ namespace XFlag.Alter3Simulator
         {
             try
             {
-                _commandParser.ParseCommandLine(_requestContext.ReceivedString).AcceptVisitor(this);
+                var command = _commandParser.ParseCommandLine(_requestContext.ReceivedString);
                 _requestContext.ResponseWriter.WriteLine(Response.OK);
             }
             catch (Exception e)
@@ -37,141 +35,116 @@ namespace XFlag.Alter3Simulator
             }
         }
 
-        public override void Visit(RecordMotionCommand command)
+        private void ProcessCommand(ICommand originalCommand)
         {
-            _coreSystem.IsRecording = !command.IsStop;
-        }
-
-        public override void Visit(IsRecordingMotionCommand command)
-        {
-            _requestContext.ResponseWriter.WriteLine(_coreSystem.IsRecording ? StatusText.Recording : StatusText.NotRecording);
-        }
-
-        public override void Visit(RobotInfoCommand command)
-        {
-            _requestContext.ResponseWriter.WriteLine("DummyRobot 0 0");
-        }
-
-        public override void Visit(ResetPoseCommand command)
-        {
-        }
-
-        public override void Visit(HelloCommand command)
-        {
-            _coreSystem.SetClientName(_requestContext.ClientId, command.ClientName);
-        }
-
-        public override void Visit(QuitCommand command)
-        {
-            _requestContext.IsClose = true;
-        }
-
-        public override void Visit(IsConnectedCommand command)
-        {
-            _requestContext.ResponseWriter.WriteLine(_coreSystem.IsRobotConnected ? StatusText.Connected : StatusText.NotConnected);
-        }
-
-        public override void Visit(ConnectRobotCommand command)
-        {
-            _coreSystem.IsRobotConnected = true;
-        }
-
-        public override void Visit(DisconnectRobotCommand command)
-        {
-            _coreSystem.IsRobotConnected = false;
-        }
-
-        public override void Visit(PrintQueueCommand command)
-        {
-        }
-
-        public override void Visit(ClearQueueCommand command)
-        {
-        }
-
-        public override void Visit(WhoAmICommand command)
-        {
-            _requestContext.ResponseWriter.WriteLine(_coreSystem.GetClient(_requestContext.ClientId).ToString());
-        }
-
-        public override void Visit(ClientsInfoCommand command)
-        {
-            foreach (var client in _coreSystem.Clients)
+            switch (originalCommand)
             {
-                _requestContext.ResponseWriter.WriteLine(client.ToString());
-            }
-        }
-
-        public override void Visit(GetAxisCommand command)
-        {
-            if (command.AxisNumber == 0)
-            {
-                var buf = new StringBuilder();
-                for (int i = 0; i < _coreSystem.Robot.AxisCount; ++i)
-                {
-                    if (i != 0)
+                case RecordMotionCommand command:
                     {
-                        buf.Append(' ');
+                        _coreSystem.IsRecording = !command.IsStop;
                     }
-                    buf.Append(_coreSystem.Robot.GetAxis(i + 1));
-                }
-                _requestContext.ResponseWriter.WriteLine(buf.ToString());
+                    break;
+                case IsRecordingMotionCommand _:
+                    {
+                        _requestContext.ResponseWriter.WriteLine(_coreSystem.IsRecording ? StatusText.Recording : StatusText.NotRecording);
+                    }
+                    break;
+                case RobotInfoCommand _:
+                    {
+                        _requestContext.ResponseWriter.WriteLine("DummyRobot 0 0");
+                    }
+                    break;
+                case HelloCommand command:
+                    {
+                        _coreSystem.SetClientName(_requestContext.ClientId, command.ClientName);
+                    }
+                    break;
+                case QuitCommand _:
+                    {
+                        _requestContext.IsClose = true;
+                    }
+                    break;
+                case IsConnectedCommand _:
+                    {
+                        _requestContext.ResponseWriter.WriteLine(_coreSystem.IsRobotConnected ? StatusText.Connected : StatusText.NotConnected);
+                    }
+                    break;
+                case ConnectRobotCommand _:
+                    {
+                        _coreSystem.IsRobotConnected = true;
+                    }
+                    break;
+                case DisconnectRobotCommand _:
+                    {
+                        _coreSystem.IsRobotConnected = false;
+                    }
+                    break;
+                case WhoAmICommand _:
+                    {
+                        _requestContext.ResponseWriter.WriteLine(_coreSystem.GetClient(_requestContext.ClientId).ToString());
+                    }
+                    break;
+                case ClientsInfoCommand _:
+                    foreach (var client in _coreSystem.Clients)
+                    {
+                        _requestContext.ResponseWriter.WriteLine(client.ToString());
+                    }
+                    break;
+                case GetAxisCommand command:
+                    if (command.AxisNumber == 0)
+                    {
+                        var buf = new StringBuilder();
+                        for (int i = 0; i < _coreSystem.Robot.AxisCount; ++i)
+                        {
+                            if (i != 0)
+                            {
+                                buf.Append(' ');
+                            }
+                            buf.Append(_coreSystem.Robot.GetAxis(i + 1));
+                        }
+                        _requestContext.ResponseWriter.WriteLine(buf.ToString());
+                    }
+                    else
+                    {
+                        var axisValue = _coreSystem.Robot.GetAxis(command.AxisNumber);
+                        _requestContext.ResponseWriter.WriteLine(axisValue.ToString());
+                    }
+                    break;
+                case MoveAxisCommand command:
+                    {
+                        _coreSystem.Robot.MoveAxis(command.Param);
+                    }
+                    break;
+                case MoveAxesCommand command:
+                    {
+                        _coreSystem.Robot.MoveAxes(command.Params);
+                    }
+                    break;
+                case GetPositionsCommand _:
+                    {
+                        var positions = _coreSystem.Robot.GetHandsPositionArray();
+                        var first = true;
+                        foreach (var position in positions)
+                        {
+                            if (first)
+                            {
+                                first = false;
+                            }
+                            else
+                            {
+                                _requestContext.ResponseWriter.Write(' ');
+                            }
+
+                            _requestContext.ResponseWriter.Write(position.x);
+                            _requestContext.ResponseWriter.Write(' ');
+                            _requestContext.ResponseWriter.Write(position.y);
+                            _requestContext.ResponseWriter.Write(' ');
+                            _requestContext.ResponseWriter.Write(position.z);
+                        }
+                        _requestContext.ResponseWriter.WriteLine();
+                    }
+                    break;
             }
-            else
-            {
-                var axisValue = _coreSystem.Robot.GetAxis(command.AxisNumber);
-                _requestContext.ResponseWriter.WriteLine(axisValue.ToString());
-            }
-        }
-
-        public override void Visit(AppendAxisCommand command)
-        {
-        }
-
-        public override void Visit(AddAxisCommand command)
-        {
-        }
-
-        public override void Visit(MoveAxisCommand command)
-        {
-            _coreSystem.Robot.MoveAxis(command.Param);
-        }
-
-        public override void Visit(MoveAxesCommand command)
-        {
-            _coreSystem.Robot.MoveAxes(command.Params);
-        }
-
-        public override void Visit(PlayMotionCommand command)
-        {
-        }
-
-        public override void Visit(GetPositionsCommand command)
-        {
-            var positions = _coreSystem.Robot.GetHandsPositionArray();
-            var first = true;
-            foreach (var position in positions)
-            {
-                if (first)
-                {
-                    first = false;
-                }
-                else
-                {
-                    _requestContext.ResponseWriter.Write(' ');
-                }
-
-                _requestContext.ResponseWriter.Write(position.x);
-                _requestContext.ResponseWriter.Write(' ');
-                _requestContext.ResponseWriter.Write(position.y);
-                _requestContext.ResponseWriter.Write(' ');
-                _requestContext.ResponseWriter.Write(position.z);
-            }
-            _requestContext.ResponseWriter.WriteLine();
-        }
-
-        protected internal override void Default(ICommand command)
-        {
         }
     }
 }

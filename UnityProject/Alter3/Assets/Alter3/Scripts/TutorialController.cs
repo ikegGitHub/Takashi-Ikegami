7ba01a6ceea5ae18e0270c5b7b702d6980ead353
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-03-09 00:00:51 +0900

    初回起動時にチュートリアルを表示

---
+++ UnityProject/Alter3/Assets/Alter3/Scripts/TutorialController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace XFlag.Alter3Simulator
+{
+    [DisallowMultipleComponent]
+    public class TutorialController : MonoBehaviour, IPointerClickHandler
+    {
+        private const string FlagKey = "tutorialShown";
+
+        [SerializeField]
+        private Animator _animator = null;
+
+        private void Awake()
+        {
+            var tutorialShownFlag = PlayerPrefs.GetInt(FlagKey, 0);
+            if (tutorialShownFlag == 0)
+            {
+                PlayerPrefs.SetInt(FlagKey, 1);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void OnFinished()
+        {
+            Destroy(gameObject);
+        }
+
+        void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+        {
+            _animator.SetTrigger("Next");
+        }
+    }
+}
